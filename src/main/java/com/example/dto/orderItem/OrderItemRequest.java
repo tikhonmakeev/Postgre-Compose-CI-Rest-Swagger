f7aea61c82879d8cb4d5334a9fd499f03eda6c12
tikhonmakeev <tihon.makeev@yandex.ru>
2025-03-31 17:45:55 +0300

    added getter annotation in OrderItemRequest

--- src/main/java/com/example/dto/orderItem/OrderItemRequest.java
+++ src/main/java/com/example/dto/orderItem/OrderItemRequest.java
@@ -3,8 +3,10 @@ package com.example.dto.orderItem;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
+@Getter
 public class OrderItemRequest {
     @NotNull(message = "Product ID is required")
     private Long productId;
