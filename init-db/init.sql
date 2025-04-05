f402c6bc99632161a6a60f079c4081df55232a48
tikhonmakeev <tihon.makeev@yandex.ru>
2025-04-05 23:11:26 +0300

    changed DB field 'description' to not null

--- init-db/init.sql
+++ init-db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     category VARCHAR(100)
 );
